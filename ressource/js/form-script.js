278aa5602b422cf7762a3f4720cdab7493c0811c
l4ur4nn3 <nicolaslauranne99@gmail.com>
2022-11-08 15:18:05 +0100

    wip form -- img int OK

---
+++ ressource/js/form-script.js
@@ -0,0 +1,15 @@
+// const fileInput = document.getElementById('file-input');
+
+// fileInput.addEventListener('change', (e) =>
+//     doSomethingWithFiles(e.target.files)
+// );
+
+const image_input = document.querySelector("#image-input");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
+  });
+  reader.readAsDataURL(this.files[0]);
+});
