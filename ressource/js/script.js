178e0a83e246209b6f6ac29daeadabde1768b2a2
Sephirah <nicolas.bussiere@outlook.fr>
2022-11-09 12:07:53 +0100

    Finished index.html

--- ressource/js/script.js
+++ ressource/js/script.js
@@ -13,20 +13,27 @@ async function getChara() {
         let image = document.createElement("img"); //2ème étape
         let name = document.createElement("h2");
         let littledesc = document.createElement("p");
+        let buttonCharacter = document.createElement("p");
+        let characterLink = document.createElement("a");
 
         //Set attribute and content
         card.setAttribute("class", "card bg-slate-500 rounded-2xl");
         image.setAttribute("src", "data:image/gif;base64," + elem.image);//3ème étape
-        image.setAttribute("class", "py-5 rounded-full inline-block w-16");
-        name.setAttribute("class", "text-2xl")
-        littledesc.setAttribute("class", "py-5");
+        image.setAttribute("class", "my-5 rounded-full inline-block w-20");
+        name.setAttribute("class", "text-2xl font-bold")
+        littledesc.setAttribute("class", "my-5");
+        characterLink.setAttribute("href", "#");
+        buttonCharacter.setAttribute("class", "rounded-full bg-cyan-600 mb-5 py-3 px-3 inline-block");
         name.textContent = elem.name;
         littledesc.textContent = elem.shortDescription;
+        buttonCharacter.textContent = "View the Character Panel";
 
         //Append child
         card.appendChild(image);//4éme étape (document.body.appendChild(image))
         card.appendChild(name);
         card.appendChild(littledesc);
+        characterLink.appendChild(buttonCharacter)
+        card.appendChild(characterLink);
         wrapper.appendChild(card);
       }
 
