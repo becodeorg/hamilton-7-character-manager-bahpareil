49c3e6c4465802d94bcc3dfa79ac5a64d0b49d39
Sephirah <nicolas.bussiere@outlook.fr>
2022-11-09 10:40:16 +0100

    Basic css of index.html

--- ressource/js/script.js
+++ ressource/js/script.js
@@ -8,17 +8,22 @@ async function getChara() {
       const response = await axios.get("https://character-database.becode.xyz/characters");
       console.log(response);
       for(elem of response.data) {
+        //Create element
         let card = document.createElement("div");
         let image = document.createElement("img"); //2ème étape
         let name = document.createElement("h2");
         let littledesc = document.createElement("p");
 
-        card.setAttribute("class", "card");
+        //Set attribute and content
+        card.setAttribute("class", "card bg-slate-500 rounded-2xl");
         image.setAttribute("src", "data:image/gif;base64," + elem.image);//3ème étape
-        image.setAttribute("class", "rounded-full w-10");
+        image.setAttribute("class", "py-5 rounded-full inline-block w-16");
+        name.setAttribute("class", "text-2xl")
+        littledesc.setAttribute("class", "py-5");
         name.textContent = elem.name;
         littledesc.textContent = elem.shortDescription;
 
+        //Append child
         card.appendChild(image);//4éme étape (document.body.appendChild(image))
         card.appendChild(name);
         card.appendChild(littledesc);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "./ressource/js/script.js"],
   theme: {
     extend: {},
   },
