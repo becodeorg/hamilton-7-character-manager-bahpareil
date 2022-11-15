feda3f5e22bc890bb2ce4e84872eb486d6b62489
Sephirah <nicolas.bussiere@outlook.fr>
2022-11-15 10:03:52 +0100

    Finished form

--- ressource/js/form-script.js
+++ ressource/js/form-script.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const image_input = document.getElementById("image-input");
+let uploaded_image;
+let upl_img_array = [];
 let display_image = document.getElementById("displayImage");
 const create = document.getElementById("create-character");
-const del = document.getElementById("delete-character");
 const desc = document.getElementById("x");
+const desc_editor = document.getElementById("desc_editor");
 const name = document.getElementById("firstname");
 const shortDesc = document.getElementById("short-description")
 let uploaded_img = document.getElementById("uploaded_img");
@@ -15,35 +17,28 @@ let imgUrl = "";
 image_input.addEventListener("change", function() {
     const reader = new FileReader();
     reader.readAsDataURL(this.files[0]);
-    //imgUrl = reader.readAsDataURL(this.files[0]);
 
     reader.addEventListener("load", () => {
-        const uploaded_image = reader.result;
+        uploaded_image = reader.result;
         image.setAttribute("src", `${uploaded_image}`);
         image.setAttribute("class", "mx-auto rounded-full w-28 h-28");
         image.setAttribute("id", "uploaded_img");
         display_image.appendChild(image);
+        upl_img_array = uploaded_image.split(",");
+        console.log(upl_img_array[1]);
     });   
 });
 
 create.addEventListener("click", createCharacter)
-del.addEventListener("click", delCharacter);
-
-function delCharacter(e) {
-    desc.value="";
-    name.value="";
-    shortDesc.value="";
-    image_input.value="";
-}
-
 
 function createCharacter(e) {
-    character.description = desc.value;
-    character.image = uploaded_img.readAsDataURL;
+    character.description = desc.value.replace( /(<([^>]+)>)/ig, '');
+    character.image = upl_img_array[1];
     character.name = name.value;
     character.shortDescription = shortDesc.value;
 
     sendCharacter();
+    alert("Character saved!");
 }
 
 const sendCharacter = async () => {
